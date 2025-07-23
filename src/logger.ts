f050bfe6eebe9c545bfd08be6299c6c3a6409dd1
Murat Erkin Cicek <murerkinn@gmail.com>
2025-07-23 15:04:57 +0300

    Remove pino-pretty

--- src/logger.ts
+++ src/logger.ts
@@ -3,13 +3,7 @@ import pino from 'pino'
 export const createLogger = () =>
   pino({
     name: 'openserv-agent',
-    level: process.env.LOG_LEVEL || 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true
-      }
-    }
+    level: process.env.LOG_LEVEL || 'info'
   })
 
 export const logger = createLogger()
